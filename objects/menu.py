088b43e131be8cca3f726ec5008daf7a36e47c16
atnartur <i@atnartur.ru>
2017-10-29 13:45:18 +0300

    + help, button

--- objects/menu.py
+++ objects/menu.py
@@ -3,8 +3,28 @@ from objects.base import Base
 from objects.car import Car
 from objects.text import Text
 from objects.simple import Simple
+from settings import colors
 
 class Menu(Base):
+
+    class Button(Base):
+        def __init__(self, text, offset, is_active=False):
+            self.sizes = (600, 100)
+            self.offset = offset
+            self.is_active = is_active
+            x, y = offset
+            self.text = Text(text, offset=(x, y), size=30, type=Text.TYPE__BOLD)
+
+        def update(self, screen):
+            x, y = self.offset
+            x_size, y_size = self.sizes
+            draw.rect(screen, colors['blue'], (x - x_size / 2, y - y_size / 2, x_size, y_size))
+            self.text.update(screen)
+
+        def render(self, screen):
+            pass
+
+
     def __init__(self, screen):
         w, h = screen.get_size()
 
@@ -12,7 +32,15 @@ class Menu(Base):
         self.objects = [
             Car(w / 2 - 100, first_line),
             Text("PyGame", offset=(w / 2 + 30, first_line), size=50, type=Text.TYPE__BOLD),
-            Simple(lambda screen: draw.line(screen, (0, 153, 254), (0, 560), (screen.get_size()[0], 560), 2)),
+
+            Menu.Button("Начать игру", (w / 2, 300)),
+
+            Text("Горячие клавиши", offset=(w / 2, 450), size=20, type=Text.TYPE__BOLD),
+            Text("Q, Esc - выход", offset=(w / 2, 480), size=20),
+            Text("Enter - начать игру", offset=(w / 2, 500), size=20),
+
+            Simple(lambda screen: draw.line(screen, colors['blue'], (0, 560), (screen.get_size()[0], 560), 2)),
+
             Text("© 2017 Булат Гиниятуллин, Артур Атнагулов", offset=(w / 2, 580), size=17),
         ]
         pass
