126983a7305dc33a6df35b7fc8ad355cebca5f8f
atnartur <i@atnartur.ru>
2017-11-01 22:30:25 +0300

    ! позиция машинки

--- game.py
+++ game.py
@@ -7,6 +7,5 @@ class Game:
         self.objects = objects
 
     def start(self):
-        self.objects.append(Car(width / 2 - 100, height / 2))
-        self.objects.append(Car(width / 2 + 100, height / 2, Car.COLOR__RED))
+        self.objects.append(Car(width / 2, height - 60))
         self.objects.append(Walls((width, height - 50)))
