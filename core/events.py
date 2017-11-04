f3cc5da06857d1691bdbf2473276c29a176128d1
Bulat Giniiatullin <bulat.giniiatullin@gmail.com>
2017-11-05 00:09:51 +0300

    ! EventsHandler class deleted, is_collision as method

--- core/events.py
+++ core/events.py
@@ -1,28 +1,23 @@
-class EventsHandler:
-    def __init__(self, car, walls):
-        self.car = car
-        self.walls = walls
-
-    def is_collision(self):
-        screen_w = self.walls.size[0]
-        car_x, car_y = self.car.x, self.car.y
-        car_w, car_h = self.car.img.get_width(), self.car.img.get_height()
-        is_collision = False
-        i = 0
-        while i < len(self.walls.coordinates) and not is_collision:
-            y, width, direction = self.walls.coordinates[i]
-            in_width = False
-            in_height = False
-            if car_y - car_h/2 <= y <= car_y + car_h/2:
-                in_height = True
-            else:
-                continue
-            if direction and (car_x - car_w/2 <= width):
-                in_width = True
-            elif not direction and (car_x + car_w/2 >= screen_w - width):
-                in_width = True
-            else:
-                continue
-            is_collision = in_height and in_width
-        return is_collision
+def is_collision(car, walls):
+    screen_w = walls.size[0]
+    car_x, car_y = car.x, car.y
+    car_w, car_h = car.img.get_width(), car.img.get_height()
+    collision = False
+    i = 0
+    while i < len(walls.coordinates) and not collision:
+        y, width, direction = walls.coordinates[i]
+        in_width = False
+        in_height = False
+        if car_y - car_h/2 <= y <= car_y + car_h/2:
+            in_height = True
+        else:
+            continue
+        if direction and (car_x - car_w/2 <= width):
+            in_width = True
+        elif not direction and (car_x + car_w/2 >= screen_w - width):
+            in_width = True
+        else:
+            continue
+        collision = in_height and in_width
+    return collision
 
