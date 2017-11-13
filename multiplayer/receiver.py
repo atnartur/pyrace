74f84df6dd1dcea8038768dbe9fb44e485513b88
atnartur <i@atnartur.ru>
2017-11-13 07:13:38 +0300

    ! позиция удаленной машинки

--- multiplayer/receiver.py
+++ multiplayer/receiver.py
@@ -1,5 +1,4 @@
-from objects.game.final import Final
-from settings import Direction
+from settings import Direction, width
 from settings import walls_acceleration_coefficient, car_acceleration_coefficient
 
 
@@ -16,29 +15,29 @@ class Receiver:
             self.player.walls.coordinates.append(wall)
 
     def move(self, data):
-        self.player.car.x = data['car_x']
+        self.player.car.x = width + data['car_x']
         self.player.car.direction = Direction(data['direction'])
 
     def score(self, data):
-        self.player.car.x = data['car_x']
+        self.player.car.x = width + data['car_x']
         self.player.info_panel.score = data['val']
 
     def acc(self, data):
-        self.player.car.x = data['car_x']
+        self.player.car.x = width + data['car_x']
         self.player.car.is_accelerated = True
         self.player.car.acceleration_coefficient = car_acceleration_coefficient
 
     def acc_stop(self, data):
-        self.player.car.x = data['car_x']
+        self.player.car.x = width + data['car_x']
         self.player.car.is_accelerated = False
 
     def wall_acc(self, data):
-        self.player.car.x = data['car_x']
+        self.player.car.x = width + data['car_x']
         self.player.walls.is_accelerated = True
         self.player.walls.acceleration_coefficient = walls_acceleration_coefficient
 
     def wall_acc_stop(self, data):
-        self.player.car.x = data['car_x']
+        self.player.car.x = width + data['car_x']
         self.player.walls.is_accelerated = False
 
     def end(self, data):
